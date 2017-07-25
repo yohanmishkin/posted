2f442b3d681618c50410d9aa24a717b7df496e0a
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-07-25 16:46:15 -0400

    Added StampTo, StampCc, StampBcc, StampSubject

--- Posted/Stamp.cs
+++ Posted/Stamp.cs
@@ -6,4 +6,64 @@ namespace Posted
     {
         void Attach(MailMessage message);
     }
+
+    public sealed class StampTo : Stamp
+    {
+        private readonly string _recipient;
+
+        public StampTo(string recipient)
+        {
+            _recipient = recipient;
+        }
+
+        public void Attach(MailMessage message)
+        {
+            message.To.Add(_recipient);
+        }
+    }
+
+    public sealed class StampCc : Stamp
+    {
+        private readonly string _recipient;
+
+        public StampCc(string recipient)
+        {
+            _recipient = recipient;
+        }
+
+        public void Attach(MailMessage message)
+        {
+            message.CC.Add(_recipient);
+        }
+    }
+
+    public sealed class StampBcc : Stamp
+    {
+        private readonly string _recipient;
+
+        public StampBcc(string recipient)
+        {
+            _recipient = recipient;
+        }
+
+        public void Attach(MailMessage message)
+        {
+            message.Bcc.Add(_recipient);
+        }
+    }
+
+    public sealed class StampSubject : Stamp
+    {
+        private readonly string _subject;
+
+        public StampSubject(string subject)
+        {
+            _subject = subject;
+        }
+
+        public void Attach(MailMessage message)
+        {
+            message.Subject = _subject;
+        }
+    }
 }
