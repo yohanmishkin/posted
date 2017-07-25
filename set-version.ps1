6440f71f18e3920031ff78b9183dc507ab793fe4
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-07-25 17:08:05 -0400

    Added versioning for nuget package

---
+++ set-version.ps1
@@ -0,0 +1,18 @@
+$isRelease = $Env:APPVEYOR_REPO_BRANCH -match 'release-v.+';
+
+$project = 'Posted';
+$path = Resolve-Path "$project/$project.nuspec";
+
+Write-Host "Updating nuget version for project $project";
+
+[xml]$nuspec = Get-Content $path;
+if ($isRelease) {
+  $nuspec.package.metadata.version = '$version$';
+  Write-Host 'Set version to "$version$"';
+} else {
+  $nuspec.package.metadata.version = '$version$-beta';
+  Write-Host 'Set version to "$version$"-beta';
+}
+
+$nuspec.Save($path);
+Write-Host "Saved file $path";
