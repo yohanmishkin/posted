b97dd0508a4cac0fed2743955e10fd00cdc5f768
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-07-26 10:35:28 -0400

    Updating nuget versioning

--- set-version.ps1
+++ set-version.ps1
@@ -1,3 +1,4 @@
+$version = $Env:APPVEYOR_BUILD_VERSION;
 $isRelease = $Env:APPVEYOR_REPO_BRANCH -match 'release-v.+';
 
 $project = 'Posted';
@@ -7,11 +8,11 @@ Write-Host "Updating nuget version for project $project";
 
 [xml]$nuspec = Get-Content $path;
 if ($isRelease) {
-  $nuspec.package.metadata.version = '$version$';
-  Write-Host 'Set version to "$version$"';
+  $nuspec.package.metadata.version = '$version';
+  Write-Host 'Set version to "$version"';
 } else {
-  $nuspec.package.metadata.version = '$version$-beta';
-  Write-Host 'Set version to "$version$"-beta';
+  $nuspec.package.metadata.version = '$version-beta';
+  Write-Host 'Set version to "$version"-beta';
 }
 
 $nuspec.Save($path);
