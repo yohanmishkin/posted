db5abaf925cda45848e56b7b47fea4a79fecf5da
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-07-26 17:22:23 -0400

    Added StampFrom

---
+++ Posted/Stamp/StampFrom.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Mail;
+
+namespace Posted
+{
+    public sealed class StampFrom : Stamp
+    {
+        private readonly string _sender;
+
+        public StampFrom(string sender)
+        {
+            _sender = sender;
+        }
+
+        public void Attach(MailMessage message)
+        {
+            message.To.Add(_sender);
+        }
+    }
+}
