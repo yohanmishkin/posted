608bfc18478908e6a0421f55012529e62eccd280
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-07-26 11:02:06 -0400

    Implemented SmtpWire

--- Posted/Wire.cs
+++ Posted/Wire.cs
@@ -1,9 +1,25 @@
 ﻿using System.Net.Mail;
+using System.Threading;
 
 namespace Posted
 {
     public interface Wire
     {
         SmtpClient Connect();
+    }
+
+    public sealed class SmtpWire : Wire
+    {
+        private readonly string _host;
+
+        public SmtpWire(string host)
+        {
+            _host = host;
+        }
+
+        public SmtpClient Connect()
+        {
+            return new SmtpClient(_host);
+        }
     }
 }
