abdce8a8c73e5da1acc6252a374fbac40b5dd158
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-09-27 10:52:06 -0400

    Giving more control over SmtpWire default credentials usage

--- Posted/Wires/SmtpWire.cs
+++ Posted/Wires/SmtpWire.cs
@@ -5,17 +5,26 @@ namespace Posted
     public sealed class SmtpWire : Wire
     {
         private readonly string _host;
+        private readonly bool _useDefaultCredentials;
 
-        public SmtpWire(string host)
+        public SmtpWire(string host, bool useDefaultCredentials)
         {
             _host = host;
+            _useDefaultCredentials = useDefaultCredentials;
+        }
+
+        public SmtpWire(string host) : this(host, true)
+        {
         }
 
         public SmtpWire() { }
 
         public SmtpClient Connect()
         {
-            return new SmtpClient(_host);
+            return new SmtpClient(_host)
+            {
+                UseDefaultCredentials = _useDefaultCredentials
+            };
         }
     }
 }
